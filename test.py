626058303fe3c2c2162d1e9715243c7def7363c0
Petar Vukovic <pvukovic@raf.edu.yu>
2021-03-22 18:25:32 +0100

    new features

--- test.py
+++ test.py
@@ -2,9 +2,7 @@ import http.client
 
 conn = http.client.HTTPSConnection("deckofcardsapi.com")
 payload = ''
-headers = {
-  'Cookie': '__cfduid=d2575d61e486c6f79fff404b270f6bf961615830988'
-}
+headers = {}
 conn.request("GET", "/api/deck/nydqb33sezct/draw/?count=2%0A", payload, headers)
 res = conn.getresponse()
 data = res.read()
